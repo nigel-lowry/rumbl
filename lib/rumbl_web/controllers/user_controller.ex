b888b7bb79342c3a4c90a89f6683b7c0f7d85bde
Nigel Lowry <nigel@lemmata.co.uk>
2020-08-16 14:05:09 +0100

    Integrate videos resource

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -4,7 +4,7 @@ defmodule RumblWeb.UserController do
   alias Rumbl.Accounts
   alias Rumbl.Accounts.User
 
-  plug :authenticate when action in [:index, :show]
+  plug :authenticate_user when action in [:index, :show]
 
   def new(conn, _params) do
     changeset = Accounts.change_registration(%User{}, %{})
@@ -30,17 +30,6 @@ defmodule RumblWeb.UserController do
         |> redirect(to: Routes.user_path(conn, :index))
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
-    end
-  end
-
-  defp authenticate(conn, _opts) do
-    if conn.assigns.current_user do
-      conn
-    else
-      conn
-      |> put_flash(:error, "You must be logged in to access that page")
-      |> redirect(to: Routes.page_path(conn, :index))
-      |> halt()
     end
   end
 end
