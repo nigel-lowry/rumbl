d862b2f80b3e83b774d2b3b310f8333f78e050d3
Nigel Lowry <nigel@lemmata.co.uk>
2021-02-03 11:31:01 +0000

    Add tests

--- test/rumbl_web/controllers/auth_test.exs
+++ test/rumbl_web/controllers/auth_test.exs
@@ -44,4 +44,19 @@ defmodule RumblWeb.AuthTest do
     next_conn = get(logout_conn, "/")
     refute get_session(next_conn, :user_id)
   end
+
+  test "call places user from session into assigns", %{conn: conn} do
+    user = user_fixture()
+    conn =
+      conn
+      |> put_session(:user_id, user.id)
+      |> Auth.call(Auth.init([]))
+
+    assert conn.assigns.current_user.id == user.id
+  end
+
+  test "call with no session sets current_user assign to nil", %{conn: conn} do
+    conn = Auth.call(conn, Auth.init([]))
+    assert conn.assigns.current_user == nil
+  end
 end
